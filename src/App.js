49278d72d4141e72fd01c8b9078b153f14b99c73
destalaz <38238206+destalaz@users.noreply.github.com>
2018-11-06 16:40:59 +0700

    55

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 //import Main from './Components/Main'
-import semiMain from './Components/semiMain';
+import SemiMain from './Components/semiMain';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <semiMain/>
+        <SemiMain/>
       </div>
     );
   }
